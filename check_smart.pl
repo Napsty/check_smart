ce5956aa7bafc9183b9ad155437d70a037098b2f
Napsty <ck@claudiokuenzler.com>
2014-05-05 15:30:28 +0200

    Bouncing to version 5.2

--- check_smart.pl
+++ check_smart.pl
@@ -19,6 +19,7 @@
 # Mar 19, 2014: Claudio Kuenzler - bugfix in defect list perfdata (rev 4.2)
 # Apr 22, 2014: Jerome Lauret - implemented -g to do a global lookup (rev 5.0)
 # Apr 25, 2014: Claudio Kuenzler - cleanup, merge Jeromes code, perfdata output fix (rev 5.1)
+# May 5, 2014: Caspar Smit - Fixed output bug in global check / issue #3 (rev 5.2)
 
 use strict;
 use Getopt::Long;
@@ -26,7 +27,7 @@ use Getopt::Long;
 use File::Basename qw(basename);
 my $basename = basename($0);
 
-my $revision = '$Revision: 5.1 $';
+my $revision = '$Revision: 5.2 $';
 
 use FindBin;
 use lib $FindBin::Bin;

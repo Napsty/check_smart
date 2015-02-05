559126a292233975d8f0d3663a351e65ba376e9d
Napsty <ck@claudiokuenzler.com>
2015-02-05 17:40:57 +0100

    Version 5.4

--- check_smart.pl
+++ check_smart.pl
@@ -21,6 +21,7 @@
 # Apr 25, 2014: Claudio Kuenzler - cleanup, merge Jeromes code, perfdata output fix (rev 5.1)
 # May 5, 2014: Caspar Smit - Fixed output bug in global check / issue #3 (rev 5.2)
 # Feb 4, 2015: Caspar Smit and cguadall - Allow detection of more than 26 devices / issue #5 (rev 5.3)
+# Feb 5, 2015: Bastian de Groot - Different ATA vs. SCSI lookup (rev 5.4)
 
 use strict;
 use Getopt::Long;
@@ -28,7 +29,7 @@ use Getopt::Long;
 use File::Basename qw(basename);
 my $basename = basename($0);
 
-my $revision = '$Revision: 5.3 $';
+my $revision = '$Revision: 5.4 $';
 
 use FindBin;
 use lib $FindBin::Bin;

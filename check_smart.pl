deb6d81176b78eead0db091768c87c15d8cdb3b5
Napsty <ck@claudiokuenzler.com>
2019-11-29 13:20:15 +0100

    Bump version

--- check_smart.pl
+++ check_smart.pl
@@ -39,13 +39,14 @@
 # Oct 1, 2019: Michael Krahe - Allow exclusion from perfdata as well (-E) and by attribute number (6.3)
 # Oct 29, 2019: Jesse Becker - Remove dependency on utils.pm, add quiet parameter (6.4)
 # Nov 22, 2019: Claudio Kuenzler - Add Reported_Uncorrect and Reallocated_Event_Count to default raw list (6.5)
+# Nov 29, 2019: Claudio Kuenzler - Add 3ware and cciss devices for global (-g) check, adjust output (6.6)
 
 use strict;
 use Getopt::Long;
 use File::Basename qw(basename);
 
 my $basename = basename($0);
-my $revision = '6.5';
+my $revision = '6.6';
 
 # Standard Nagios return codes
 my %ERRORS=('OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4);
